58c684c68c1fac67b8d58443773ae712455f9393
Yossifemad19 <yossifemad22@gmail.com>
2024-12-13 22:18:55 +0200

    connect ticket with gui

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -2,62 +2,81 @@
 open System.Windows.Forms
 open System.Drawing
 
+open Ticket
 
 [<EntryPoint>]
 let main argv=
     
-    let Display_Seats(panal:Panel,n:int)=
-            let mutable x=0
-            let mutable y=0
-            for i in 0..n do
-                let groupBox =new GroupBox(
-                    Text = $"seat {i+1}",
-                    Width=200,
-                    Height=125,
-                    Location=new Point(x,y)
-                )
-                let bookButton = new Button(
-                    Text="Book",
-                    Width=100,
-                    Height=30,
-                    Location=new Point(50,85),
-                    BackColor=Color.Black,
-                    ForeColor=Color.Wheat
-                )
-                groupBox.Controls.Add(bookButton)
-                panal.Controls.Add(groupBox)
-                x <- x+200
-                if x = 800 then
-                    x <- 0
-                    y <- y+125
+    
 
     let form=new Form(Text="Cinema Reservation System",Width=1000,Height=550)
 
     let panal = new Panel(Width=800,Height=550,AccessibleName="seatScrean",BackColor=Color.LightGray)
 
-    //let daysDropDown= new ToolStripDropDownMenu(AccessibleName="daysDropDown",Width=100,Height=80,Text="day")
-    ////daysDropDown.Text <- ""
-    //daysDropDown.Location <- new Point(850,50) 
+    
+
+    let userNameInput=new TextBox(AccessibleName="UserNameInput",Width=100,
+                                    Height=80,Location=new Point(850,50),Text="userName")
 
     let daysDropDown=new ComboBox(AccessibleName="daysDropDown",Width=100,
-                                    Height=80,Location=new Point(850,50))
+                                    Height=80,Location=new Point(850,250))
 
     daysDropDown.DataSource <- [|"Select Day";"Sunday";"Monday"|]
     daysDropDown.DropDownStyle <- ComboBoxStyle.DropDownList
 
 
     let partiesDropDown=new ComboBox(AccessibleName="partiesDropDown",Width=100,
-                                    Height=80,Location=new Point(850,200))
+                                    Height=80,Location=new Point(850,300))
 
     partiesDropDown.DataSource <- [|"Select Party";"9 AM";"1 PM"|]
     partiesDropDown.DropDownStyle <- ComboBoxStyle.DropDownList
 
 
+    let Display_Seats(panal:Panel,n:int)=
+            let mutable x=0
+            let mutable y=0
+            for i in 0..3 do
+                for j in 0..3 do
+
+                    let groupBox =new GroupBox(
+                        Text = $"seat {i+1}.{j+1}",
+                        Width=200,
+                        Height=125,
+                        Location=new Point(x,y)
+                    )
+                    let bookButton = new Button(
+                        Text="Book",
+                        Width=100,
+                        Height=30,
+                        Location=new Point(50,85),
+                        BackColor=Color.Black,
+                        ForeColor=Color.Wheat
+                    )
+
+                    bookButton.Click.Add(fun _ ->
+                        let userName= userNameInput.Text
+                        if userName="userName" || userName="" then
+                            MessageBox.Show("Please provide a user name") |> ignore
+                        else
+                            let ticket = createTicket "sunday" "9 Am" (i,j) userName
+                            appendToTicketsFile ticket
+
+                    )
+
+
+                    groupBox.Controls.Add(bookButton)
+                    panal.Controls.Add(groupBox)
+                    x <- x+200
+                
+                x <- 0
+                y <- y+125
+
+
     let applyButton = new Button(AccessibleName="applyButton",Width=100,Height=30,
-                                Location=new Point(850,300),Text="Apply",BackColor=Color.Gray)
+                                Location=new Point(850,400),Text="Apply",BackColor=Color.Gray)
     applyButton.Click.Add(fun _ ->
-            let day = daysDropDown.SelectedValue
-            MessageBox.Show($"day ->{day}")
+            //let day = daysDropDown.SelectedValue
+            //MessageBox.Show($"day ->{day}")
             Display_Seats(panal,4)
     )
 
@@ -90,6 +109,8 @@ let main argv=
     //Display_Seats(panal, 4)
 
     form.Controls.Add(panal)
+    form.Controls.Add(userNameInput)
+
     form.Controls.Add(daysDropDown)
     form.Controls.Add(partiesDropDown)
     form.Controls.Add(applyButton)
---
+++ ConsoleApp1/Ticket.fs
@@ -0,0 +1,46 @@
+﻿module Ticket
+
+open System
+open System.IO
+open System.Text.Json
+
+type Ticket( day: string,party:string,seat:int*int, userName: string) =
+
+    member this.Id = Guid.NewGuid()
+    member this.Seat = seat
+    member this.Day = day
+    member this.Party = party
+    member this.UserName = userName
+
+let createTicket day party seat userName = Ticket(day,party,seat,userName)
+
+
+let appendToTicketsFile (ticket: Ticket) =
+    let filePath = "tickets.json"
+
+    let tickets =
+        if File.Exists(filePath) then
+            let json = File.ReadAllText(filePath)
+            JsonSerializer.Deserialize<Ticket list>(json)
+        else
+            []
+    let updatedTickets = ticket :: tickets
+
+    let updatedJson =
+        JsonSerializer.Serialize(updatedTickets, JsonSerializerOptions(WriteIndented = true))
+
+    File.WriteAllText(filePath, updatedJson)
+
+let readSpecificTicket (day: string) (party: string) (seat: int*int) =
+    let filePath = "tickets.json"
+
+    if File.Exists(filePath) then
+        let json = File.ReadAllText(filePath)
+        let tickets = JsonSerializer.Deserialize<Ticket list>(json)
+
+        // Filter the tickets based on the parameters
+        tickets
+        |> List.tryFind (fun ticket ->
+            ticket.Day = day && ticket.Party = party && ticket.Seat = seat)
+    else
+        None
